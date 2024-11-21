a6d00afa2c5e592353a6454eaaa63d68d5371c14
Ayalni2289 <alininspor@hotmail.com>
2024-11-21 17:30:08 +0300

    Expo komutu ile jsx formatında dosyalarım oluşturuldu.

---
+++ Pokemon_App/App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text } from 'react-native';
+
+export default function App() {
+  return (
+  <>
+    <Text>
+      
+    </Text>
+  </>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
